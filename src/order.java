2ceee67bbab9a26e0f893f7ef93eec1357924c12
c0653123 <c0653123@SB-N208-17.lambton.on.ca>
2015-05-22 09:35:30 -0400

    Order class

---
+++ src/order.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author c0653123
+ */
+public class order {
+    
+    public static int CustomerID;
+
+    public static int getCustomerID() {
+        return CustomerID;
+    }
+
+    public static void setCustomerID(int CustomerID) {
+        order.CustomerID = CustomerID;
+    }
+    
+    
+    
+    public static String CustomerName;
+
+    public static String getCustomerName() {
+        return CustomerName;
+    }
+
+    public static void setCustomerName(String CustomerName) {
+        order.CustomerName = CustomerName;
+    }
+    
+    
+    public static int TimeFullfilled;
+
+    public static int getTimeFullfilled() {
+        return TimeFullfilled;
+    }
+    public static void setTimeFullfilled(int TimeFullfilled) {
+        order.TimeFullfilled = TimeFullfilled;
+    }
+    
+    
+    public static int ProductID;
+
+    public static int getProductID() {
+        return ProductID;
+    }
+
+    public static void setProductID(int ProductID) {
+        order.ProductID = ProductID;
+    }
+    
+    
+    public static int Quantity;
+
+    public static int getQuantity() {
+        return Quantity;
+    }
+
+    public static void setQuantity(int Quantity) {
+        order.Quantity = Quantity;
+    }
+    
+    
+    public static String Notes;
+
+    public static String getNotes() {
+        return Notes;
+    }
+
+    public static void setNotes(String Notes) {
+        order.Notes = Notes;
+    }
+    
+
+    
+}
